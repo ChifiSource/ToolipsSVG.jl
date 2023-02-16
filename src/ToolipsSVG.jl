81e1081e90819f6e1e1e11dfc61242dbba99a9fc
emmy <52672675+emmettgb@users.noreply.github.com>
2023-02-16 03:08:37 -0600

    Slimmed design -- more components incoming soon

--- src/ToolipsSVG.jl
+++ src/ToolipsSVG.jl
@@ -30,8 +30,22 @@ function write!(c::AbstractConnection, svp::Component{:path})
    write!(c, svp.extras)
 end
 
+M!(path::Component{:path}, x::Number, y::Number) = path["d"] = path["d"] * "M$x $y "
+
+L!(path::Component{:path}, x::Number, y::Number) = path["d"] = path["d"] * "L$x $y "
+
+Z!(path::Component{:path}, x::Number, y::Number) = path["d"] = path["d"] * "Z"
+
+function Q!(path::Component{:path}, x::Number, a1::Number, a2::Number, y::Number)
+    path[:d] = path[:d] * "Q$x, $a1 $a2, $y "
+end
+
+function C!(x::Number, y::Number, a1::Number, a2::Number, a4::Number)
+    path[:d] = path[:d] * "C$x,$y $a1, $a2 $a3, $a4 "
+end
+
 include("SVComponents.jl")
-include("SVActions.jl")
+
 export circle, path, rect
 export M!, L!, Z!, Q!
 end # module
