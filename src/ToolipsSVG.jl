debc65c1e2bf5581c46a6e75093a0417820d3be5
emmy <52672675+emmaccode@users.noreply.github.com>
2024-04-20 01:15:21 -0600

    fixed `string(Component{:path})`, more doc
    Getting super close!

--- src/ToolipsSVG.jl
+++ src/ToolipsSVG.jl
@@ -17,46 +17,139 @@ string(comp::Component{:path}) = begin
     text::String = comp.properties[:text]
     children = string(comp[:children])
     extras = string(comp[:extras])
-    d_prop::String = "d=\"$(comp.properties["d"])\""
-    delete!(comp.properties, "d")
-    "$extras<$(comp.tag) id=\"$(comp.name)\" $(propstring(comp.properties)), $d_prop>$children$text</$(comp.tag)>"::String
+    d_prop::String = "d='$(comp["d"])'\""
+    props::String = ToolipsServables.propstring(filter(k -> k == "d", comp.properties))
+    "$extras<$(comp.tag) id=\"$(comp.name)\" $props $d_prop>$children$text</$(comp.tag)>"::String
 end
 
 """
 ```julia
-M!(path::Component{:path}, x::Number, y::Number) -> ::String
+M!(path::Component{:path}, points::Any ...) -> ::String
 ```
+`M!` adds an `M` to the `d` data of a `path`. `M` is *move* -- this moves 
+the `path` cursor to a set position.
+---
+```example
+# create our path:
+squarepath = path("new-square")
+M!(squarepath, 50, 50)
+L!(squarepath, 100, 50)
+L!(squarepath, 100, 100)
+L!(squarepath, 50, 100)
+L!(squarepath, 50, 50)
+Z!(squarepath)
+# add to svg window:
+window = svg("main", width = 500, height = 500)
+push!(window, squarepath)
+# -- display
+display("text/html", window)
+```
+```example
+cuteheart = path("heat")
+M!(cuteheart, 12, "21.593c-5.63-5.539-11-10.297-11-14.402", 
+"0-3.791 3.068-5.191 5.281-5.191 1.312", 0, "4.151.501", 
+5.719, 4.457, "1.59-3.968", "4.464-4.447",
+ "5.726-4.447", 2.54, 0, 5.274, 1.621, 5.274, 5.181, 0,
+ "4.069-5.136 8.625-11", "14.402m5.726-20.583c-2.203", "0-4.446 1.042-5.726 3.238-1.285-2.206-3.522-3.248-5.719-3.248-3.183",
+  "0-6.281", "2.187-6.281", 6.191, 0, 4.661, 5.571, 9.429, 12, 15.809, "6.43-6.38 12-11.148 12-15.809",
+   "0-4.011-3.095-6.181-6.274-6.181")
 
+window = svg("main", width = 500, height = 500)
+push!(window, cuteheart)
+display("text/html", window)
+```
+"""
+M!(path::Component{:path}, a::Any ...) = path["d"] = path["d"] * "M$(join((string(el) for el in a), " "))"
+
+"""
+```julia
+L!(path::Component{:path}, points::Any ...) -> ::String
+```
+`L!` adds an `L` to the `d` data of a `path`. `L` is *line-to*, which draws 
+a line from the current path position to the new `xy` position.
+---
+```example
+using ToolipsSVG
+# create our path:
+squarepath = path("new-square")
+M!(squarepath, 50, 50)
+L!(squarepath, 100, 50)
+L!(squarepath, 100, 100)
+L!(squarepath, 50, 100)
+L!(squarepath, 50, 50)
+Z!(squarepath)
+# add to svg window:
+window = svg("main", width = 500, height = 500)
+push!(window, squarepath)
+# -- display
+display("text/html", window)
+```
+"""
+L!(path::Component{:path}, a::Any ...) = path["d"] = path["d"] * "L$(join((string(el) for el in a), " "))"
+
+"""
+```julia
+Z!(path::Component{:path}) -> ::String
+```
+`Z!` adds a `Z` to the `d` data of a `path`. `Z` is used to close a `path`.
+---
+```example
+using ToolipsSVG
+# create our path:
+squarepath = path("new-square")
+M!(squarepath, 50, 50)
+L!(squarepath, 100, 50)
+L!(squarepath, 100, 100)
+L!(squarepath, 50, 100)
+L!(squarepath, 50, 50)
+Z!(squarepath)
+# add to svg window:
+window = svg("main", width = 500, height = 500)
+push!(window, squarepath)
+# -- display
+display("text/html", window)
+```
+"""
+Z!(path::Component{:path}) = path["d"] = path["d"] * "Z"
+
+"""
+```julia
+Q!(path::Component{:path}, a::Any ...) -> ::String
+```
+`Q!` adds a `Q` to the `d` data of a `path`.  The `Q` option 
+performs a quadratic bezier command.
 ---
 ```example
 
 ```
 """
-M!(path::Component{:path}, x::Number, y::Number) = path["d"] = path["d"] * "M$x $y "
+Q!(path::Component{:path}, a::Any ...) = path[:d] = path[:d] * "Q$(join((string(el) for el in a), " "))"
 
 """
+```julia
+C!(path::Component{:path}, a::Any ...) -> ::String
+```
+`C!` adds a `C` to the `d` data of a `path`.  The `C` option 
+performs a cubic bezier command.
+---
+```example
+
+```
+"""
+C!(path::Component{:path}, a::Any ...) = path[:d] = path[:d] * "C$(join((string(el) for el in a), " "))"
 
 """
-L!(path::Component{:path}, x::Number, y::Number) = path["d"] = path["d"] * "L$x $y "
+```julia
+A!(path::Component{:path}, a::Any ...) -> ::String
+```
+`A!` adds a `A` to the `d` data of a `path`.  The `A` option 
+performs an eliptical arch.
+---
+```example
 
+```
 """
-
-"""
-Z!(path::Component{:path}) = path["d"] = path["d"] * "Z"
-
-"""
-
-"""
-function Q!(path::Component{:path}, x::Number, a1::Number, a2::Number, y::Number)
-    path[:d] = path[:d] * "Q$x, $a1 $a2, $y "
-end
-
-"""
-
-"""
-function C!(x::Number, y::Number, a1::Number, a2::Number, a4::Number)
-    path[:d] = path[:d] * "C$x,$y $a1, $a2 $a3, $a4 "
-end
+A!(path::Component{:path}, a::Any ...) = path[:d] = path[:d] * "A$(join((string(el) for el in a), " "))"
 
 """
 ```julia
@@ -131,7 +224,11 @@ const text = Component{:text}
 const image = Component{:image}
 const circle = Component{:circle}
 const rect = Component{:rect}
-const path = Component{:path}
+
+function path(name::String, args::Pair{String, <:Any} ...; d::String = "", keyargs ...)
+    Component{:path}(name, "d" => d, args ...; keyargs ...)
+end
+
 const line = Component{:line}
 const ellipse = Component{:ellipse}
 const polyline = Component{:polyline}
